028ae362133ea2ab3be5648f6c3f4a10f68944fd
dinabogdan <bogdan.dina03@gmail.com>
2017-12-23 22:56:31 +0200

    add the DatabaseConnection class

---
+++ Programming-Engineering-Project/DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+using System.Runtime.CompilerServices;
+
+namespace Programming_Engineering_Project
+{
+	class DatabaseConnection
+	{
+		private static SQLiteConnection connection = null;
+
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		public static SQLiteConnection getConnection()
+		{
+			if (connection == null)
+			{
+				connection = new SQLiteConnection("data source=C:\\DevTools\\SQLite\\ip-db.db");
+			}
+
+			return connection;
+		}
+	}
+}
