084c18c937ae25fe44d547fa8aa6c079a14f3e61
dinabogdan <bogdan.dina03@gmail.com>
2017-12-26 10:35:52 +0200

    add validation methods for customer details

--- Programming-Engineering-Project/Utils.cs
+++ Programming-Engineering-Project/Utils.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Net.Mail;
 
 namespace Programming_Engineering_Project
 {
@@ -44,6 +45,67 @@ namespace Programming_Engineering_Project
 			}
 
 			if (customer.BirthDate == null || customer.BirthDate.Length > 0) { errors.Add("birthDateErr", "Birthdate is empty!"); }
+
+			return errors;
+		}
+		public static Dictionary<String, String> validateCustDetails(Customer customer)
+		{
+			Dictionary<String, String> errors = new Dictionary<string, string>();
+			if (customer.Phone != null && customer.Phone.Length > 0)
+			{
+				Regex regex = new Regex(@"^[0-10]{10}$");
+				if (!regex.IsMatch(customer.Phone)) { errors.Add("phoneErr", "Phone is not valid!"); }
+			}
+			else
+			{
+				errors.Add("phoneErr", "Phone is empty!");
+			}
+
+			if (customer.Email != null && customer.Email.Length > 0)
+			{
+				try
+				{
+					MailAddress m = new MailAddress(customer.Email);
+				}
+				catch (FormatException)
+				{
+					errors.Add("emailErr", "Email is not valid!");
+				}
+			}
+			else
+			{
+				errors.Add("emailErr", "The Email is empty!");
+			}
+
+			if (customer.Country != null && customer.Country.Length > 0)
+			{
+				if (!customer.Country.Equals("Romania")) { errors.Add("countryErr", "Country must be 'Romania'!"); }
+			}
+			else
+			{
+				errors.Add("countryErr", "Country is empty!");
+			}
+
+			if (!(customer.County != null && customer.County.Length > 0))
+			{
+				errors.Add("countyErr", "County is empty!");
+			}
+
+			if (customer.Locality != null && customer.Locality.Length > 0)
+			{
+				Regex regex = new Regex(@"^[a-zA-Z -]{3, 50}$");
+				if (!regex.IsMatch(customer.Locality)) { errors.Add("localityErr", "Locality is not valid!"); }
+			}
+			else
+			{
+				errors.Add("localityErr", "Locality is empty!");
+			}
+
+			if (!(customer.StreetNo > 0))
+			{
+				errors.Add("streetNo", "Street No. is empty!");
+			}
+
 
 			return errors;
 		}
