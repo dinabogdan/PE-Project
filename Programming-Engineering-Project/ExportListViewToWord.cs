8db3b618f54ae6b1e55c4e32953ea2e2fd8eacdf
dinabogdan <bogdan.dina03@gmail.com>
2018-01-06 11:30:42 +0200

    add support for creating Word Docs

---
+++ Programming-Engineering-Project/ExportListViewToWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace Programming_Engineering_Project
+{
+	class ExportListViewToWord
+	{
+		public static void createDocumentPropertyTable()
+		{
+			
+		}
+	}
+}
